12204c07182cd21b0a7c36430b4c1201f752366a
LEE DA EUN <112735794+wbwhdrhadl@users.noreply.github.com>
2024-05-21 00:06:43 +0900

    study: express rest-api

--- node/express-demo.js
+++ node/express-demo.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express()
 
+// GET + "/"
 app.get('/', function(req,res) {
     res.send('Hello World')
 })
 
-app.listen(3000)
+// API : GET + "http://localhost:1234/test"
+
+app.get('/test', function(req,res) {
+    res.send('TEST SUCCESS')
+})
+
+// API : GET + "http://localhost:1234/test/1"
+// "One!!"
+app.get('/test/1', function(req,res) {
+    res.send('One!')
+})
+
+// GET /hello, /bye, /nicetomeetyou
+app.get('/hello', function(req,res) {
+    res.send('hello')
+})
+
+app.get('/bye', function(req,res) {
+    res.send('bye')
+})
+
+app.get('/nicetomeetyou', function(req,res) {
+    res.send('nicetomeetyou')
+})
+
+app.listen(1234)
